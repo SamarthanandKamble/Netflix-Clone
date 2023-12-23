42d674a4ce21e70243f82576c4fd3b42054fcc34
Samarth1002 <samarthanandkamble1002@gmail.com>
2023-12-23 15:47:53 +0530

    Added Dynamic GPT search btn along with multi language support feature which render the input field dynamically

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,19 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { addUser, removeUser } from "../utils/Redux/userSlice";
+import { removeUser } from "../utils/Redux/userSlice";
 import { auth } from "../utils/firebase";
-import { onAuthStateChanged, signOut } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
-import { NETFLIX_LOGO } from "../utils/constants";
+import { signOut } from "firebase/auth";
+import { NETFLIX_LOGO, SUPPORTED_LANGUAGES } from "../utils/constants";
+import { gptSearchToggler } from "../utils/Redux/gptSlice";
+import useCheckForAuthChange from "../utils/Hooks/useCheckForAuthChange";
+import { updateLanguage } from "../utils/Redux/configSlice";
 
 const Header = () => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
-  const navigate = useNavigate();
+  const gptToggler = useSelector((state) => state.gpt?.gptSearchOpen);
+
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {
@@ -21,34 +24,47 @@ const Header = () => {
       });
   };
 
-  useEffect(() => {
-    
-    const subscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        const { uid, email } = user;
-        navigate("/browse");
-        dispatch(addUser({ uid: uid, email: email }));
-      } else {
-        navigate("/");
-      }
-    });
+  useCheckForAuthChange();
 
-    return () => subscribe();
-  }, []);
+  const toggleGptSearch = () => {
+    dispatch(gptSearchToggler());
+  };
+
+  const handleLanguageUpdate = (e) => {
+    dispatch(updateLanguage(e.target.value));
+  };
+
   return (
-    <div className="absolute w-full flex flex-wrap items-center justify-around border-amber-600 bg-gradient-to-b from-black">
-      <div>
-        <img src={NETFLIX_LOGO} alt="netflix-logo" className="w-48 h-45" />
+    <div className="absolute w-full flex flex-wrap items-center justify-between bg-gradient-to-b from-black px-4 top-0 z-10">
+      <div className="w-32">
+        <img src={NETFLIX_LOGO} alt="netflix-logo" className="w-full" />
       </div>
 
-      <ul className="text-white flex h-12 ">
-        <li className="p-2.5 mx-2 border rounded-md">
-          <span className="">*</span>English V
-        </li>
+      <ul className="h-10 p-2 text-white flex items-center justify-center content-center">
+        {user && (
+          <li
+            className="px-2 py-1 mx-2 font-semibold rounded-lg bg-black opacity-90 cursor-pointer"
+            onClick={toggleGptSearch}
+          >
+            {gptToggler ? "Homepage" : "GptSearch"}
+          </li>
+        )}
+        {user && gptToggler && (
+          <select
+            className="pl-2 py-1 mr-2 font-semibold rounded-md bg-black opacity-90"
+            onChange={handleLanguageUpdate}
+          >
+            {SUPPORTED_LANGUAGES.map((lang) => (
+              <option key={lang.identifier} value={lang.name}>
+                {lang.name}
+              </option>
+            ))}
+          </select>
+        )}
         {user !== null ? (
           <Link to="/">
             <li
-              className="p-2.5 border bg-red-600 rounded-md cursor-pointer"
+              className="bg-red-600 rounded-lg cursor-pointer px-2 py-1 font-semibold"
               onClick={handleSignOut}
             >
               Log Out
@@ -56,7 +72,7 @@ const Header = () => {
           </Link>
         ) : (
           <Link to="/signin">
-            <li className="p-2.5 border bg-red-600 rounded-md cursor-pointer">
+            <li className="bg-red-600 rounded-md cursor-pointer px-2 py-1 font-semibold">
               Sign In
             </li>
           </Link>
